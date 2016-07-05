93112a94ab1caeccf86eee7df81211bce0ef9ba4
ruichao yuan <ry574@nyu.edu>
2016-07-05 00:15:29 -0400

    test env

--- app.js
+++ app.js
@@ -41,7 +41,14 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
-console.log(process.env.smtpConfig);
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
@@ -55,13 +62,6 @@ if (app.get('env') === 'development') {
 }
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
 
 
 module.exports = app;
