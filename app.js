4fafa824b6f465661ab9f475ea36e846ff4e4112
ruichao yuan <ry574@nyu.edu>
2016-07-05 00:09:43 -0400

    test env

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
-
+console.log(process.env.smtpConfig);
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
@@ -53,7 +53,6 @@ if (app.get('env') === 'development') {
     });
   });
 }
-console.log(process.env);
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
